8084239b3b350eee9945bae718f41cb0a147c1cf
clcamou <clcamou@gmail.com>
2020-02-04 22:23:08 -0700

    added prompt in server.js

--- server.js
+++ server.js
@@ -18,3 +18,57 @@ connection.connect(function(err){
     } 
     promptuser();
 });
+
+function promptuser() {
+    inquirer
+        .prompt({
+            name: "action", 
+            type: "rawlist", 
+            message: "What would you like to do?", 
+            choices: [
+                "find employee", 
+                "find all employees", 
+                "find deparments", 
+                "find salaries", 
+                "find roles",
+                "view employee manager",
+                "add employee", 
+                "update employee role",
+                "update employee manager",
+                "remove employee",
+            ]
+        })
+        .then(function(answer){
+            switch (answer.action) {
+                case "find employee":
+                    employeeSearch();
+                    break;
+                case "find all employees":
+                    employeesSearch();
+                    break;
+                case "find deparments":
+                    departmentSearch();
+                    break;
+                case "find salaries":
+                    salariesSearch();
+                    break;
+                case "find roles":
+                    rolesSearch();
+                    break;
+                case "view employee manager":
+                    managerSearch();
+                case "add employee":
+                    addEmployee();
+                    break;
+                case "update employee role":
+                    updateEmployeeRole();
+                    break;
+                case "update employee manager":
+                    updateEmployeeManager();
+                    break;
+                case "remove employee":
+                    removeEmployee();
+            }
+        })
+};
+
