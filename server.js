67ae6e4709da26e3ec73943df096632af6a6a664
clcamou <clcamou@gmail.com>
2020-02-04 23:16:54 -0700

    Added javascript to prompt user

--- server.js
+++ server.js
@@ -72,3 +72,140 @@ function promptuser() {
         })
 };
 
+function employeeSearch(){
+    connection.query("SELECT * FROM employees WHERE ?", {last_name: answer.last_name}, function(err, res){
+        console.log(
+            "id: " +
+            res[0].id +
+            " || first_name: " +
+            res[0].first_name +
+            " || last_name: " +
+            res[0].last_name +
+            " || role_id: " +
+            res[0].role_id
+        );
+        runSearch();
+    })
+};
+
+function employeesSearch(){
+    connection.query("SELECT * FROM employees", 
+    function(err, res) {
+        console.log(
+            "id: " +
+            res[0].id +
+            " || first_name: " +
+            res[0].first_name +
+            " || last_name: " +
+            res[0].last_name +
+            " || role_id: " +
+            res[0].role_id +
+            " || manager_id: " +
+            res[0].manager_id
+        );
+        runSearch();
+        })
+};
+
+function departmentSearch(){
+    connection.query("SELECT * department WHERE ?", {id: answer.name}, function(err, res){
+        console.log(
+            "id: " +
+            res[0].name 
+        );
+        runSearch();
+    })
+};
+
+function salariesSearch(){
+    connection.query("SELECT * FROM role WHERE ?", {salary: answer.salary}, function(err, res){
+        console.log(
+            "id: " +
+            res[0].id +
+            "title: "+
+            res[0].title +
+            " || salary" +
+            res[0].salary +
+            "department_id: " +
+            res[0].department_id
+        );
+        runSearch();
+    })
+};
+                
+                
+function rolesSearch(){
+    connection.query("SELECT * FROM role WHERE ?", {role_id: answer.role_id}, function(err, res){
+        console.log(
+            "id: " +
+            res[0].id +
+            " || title: " +
+            res[0].title 
+        );
+        runSearch();
+    });
+};
+
+function managerSearch(){
+    connection.query("SELECT * FROM employees WHERE ?", {manager: answer.manager_id}, function(err, res){
+        console.log(
+            "id: " +
+            res[0].id +
+            " || first_name: " +
+            res[0].first_name +
+            " || last_name: " +
+            res[0].last_name +
+            " || role_id: " +
+            res[0].role_id +
+            " || manager_id : " +
+            res[0].manager_id
+        );
+        runSearch();
+    })
+};
+
+function addEmployee(){
+    connection.query("INSERT INTO employees", function(err, res){
+        inquirer.prompt([
+            {
+                type:"input", 
+                message: "What is the employee's first name?",
+                name: "first_name"
+            }, 
+            {
+                type:"input",
+                message: "What is the employee's last name?",
+                name: "last_name"
+            },
+            {
+                type: "input", 
+                message: "What is the employee's role id? ",
+                name: "role_id"
+            }, 
+            {
+                type: "input", 
+                message: "What is the manager's id?", 
+                name: "manager_id"
+            }
+        ]).then(function(response){
+            console.log(response)
+        });
+    });
+};
+
+              
+//function updateEmployee(){
+    //connection.query("UPDATE employees WHERE ?", {last_name: answer.last_name}, function(err, res){
+        
+    //})
+//};
+
+//function updateEmployeeManager(){
+  //  connection.query("UPDATE employees WHERE ?", {last_name: answer.last_name}, function(err, res){
+        
+    //})
+//};
+
+//function RemoveEmployee(){
+//   connection.query("DELETE FROM employee WHERE ?", {last_name: answer.last_name}, function(err, res){}) };
+            
